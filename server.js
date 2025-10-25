10d01675cd6fa4cd19588366eaf42470952c21c7
Anurag Singh <abc103498@gmail.com>
2025-10-25 10:00:49 +0530

    db connection setup in db.js and call it in server.js

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 import app from './app.js';
-import mongoose from 'mongoose';
+import connectDB from './config/db.js';
 import 'dotenv/config';
 
 const PORT = process.env.PORT;
-const MONGO_URI = process.env.MONGO_URI;
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected successfully');
-    app.listen(PORT, () => {
-      console.log(`Server is up and listening on PORT: ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log('Mongo connection error : ', err);
-  });
+connectDB();
+app.listen(PORT, () => {
+  console.log(`Server is up and listening on PORT: ${PORT}`);
+});
